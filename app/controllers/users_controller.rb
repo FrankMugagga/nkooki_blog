e18f042951d7e481d15903402546f8ae4a989639
FrankMugagga <frank.mugagga@gmail.com>
2024-09-23 22:25:16 +0300

    clear

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = User.all 
+    @users = User.all
   end
 
   def show
-    @user = User.find_by()
+    @user = User.find_by
   end
 
   def delete; end
