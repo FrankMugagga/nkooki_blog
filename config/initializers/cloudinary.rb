2f922197419fcf4d64578a3abe8dc847bc7ca0e0
FrankMugagga <frank.mugagga@gmail.com>
2024-11-16 10:39:14 +0300

    Correct for cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,6 @@
-Cloudinary.config do |config|
+  Cloudinary.config do |config|
     config.cloud_name = 'dbcjfpezb'
-    config.api_key = '	
-    786283298793769'
+    config.api_key = '786283298793769'
     config.api_secret = 'Sx9DIH2_TUBKnFJQnI0nfSrjP3Y'
     config.secure = true
     config.cdn_subdomain = true
